589fa895fdf0987f794354b38e4c29ec1f6f2d50
Habil <habilherdianto233z@gmail.com>
2026-06-09 09:51:43 +0700

    can search by full name or username

--- express-backend/src/controllers/userController.ts
+++ express-backend/src/controllers/userController.ts
@@ -4,7 +4,7 @@ import { prisma } from "../lib/prisma";
 export const findUser = async (req: Request, res: Response) => {
     const {input} = req.body
     try {
-        const searchedUser = await prisma.users.findMany({where: {username: {contains: input}}, omit: {password: true} ,orderBy:{created_at: "asc"}})
+        const searchedUser = await prisma.users.findMany({where: {OR: [{username: {contains: input}}, {full_name: {contains: input}}]}, omit: {password: true} ,orderBy:{created_at: "asc"}})
         console.log(searchedUser)
         return res.status(200).json({
             message: "Find user success",
