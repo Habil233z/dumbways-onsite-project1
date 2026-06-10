a28eba6cf827e06332ca6a63bcab1f1125bd7cc0
Habil <habilherdianto233z@gmail.com>
2026-06-10 12:13:27 +0700

    modify profile styling

--- react-frontend/src/pages/Profile.tsx
+++ react-frontend/src/pages/Profile.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 
@@ -20,13 +21,20 @@ export default function UserProfile() {
 
     return (
         <div className="h-screen flex bg-gray-100 flex-col w-full items-center">
-            <div className="border-gray-800 border-2 h-100 bg-white justify-center items-center mt-20 flex flex-col rounded-2xl pl-10 pr-10 shadow-4xl">
-                <h1 className="text-center font-bold text-6xl">SociNet Account</h1>
-                <div className="rounded-[50%] w-20 h-20 overflow-hidden flex justify-center items-center mt-5 border-2 border-gray-950">
-                    <img src={profile.photo_profile} className="object-none h-full"/>
+            <div className="w-[85%] border-gray-800 border-2 h-100 bg-white justify-center items-center mt-10 flex flex-col rounded-t-2xl pl-10 pr-10 shadow-4xl">
+                <div className="flex w-full">
+                    <div className="w-full">
+                        <div className="rounded-[50%] w-40 h-40 overflow-hidden flex justify-center items-center border-2 border-gray-950">
+                            <img src={profile.photo_profile} className="object-none h-full"/>
+                        </div>
+                        <h1 className="font-bold text-4xl">{profile.username}</h1>
+                        <h2 className="font-bold text-2xl">{profile.email}</h2>
+                        
+                    </div>
+                    <div className="w-full flex flex-row-reverse">
+                        <Button className="h-10 w-25 bg-blue-700 hover:bg-blue-800 active:bg-blue-900">Edit profile</Button>
+                    </div>
                 </div>
-                <h1 className="text-center font-bold text-4xl">{profile.username}</h1>
-                <h2 className="text-center font-bold text-2xl">{profile.email}</h2>
                 <p>{profile.bio}</p>
                 <p className="mt-10">Created at: {profile.created_at}</p>
             </div>
